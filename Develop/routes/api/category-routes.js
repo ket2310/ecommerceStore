3f1dd41cf01dfb46a0dc16f49b8cd47783c1be99
Kirk Thomas <dablumaroon@gmail.com>
2021-07-28 15:39:52 -0400

    Had to make minor changes to tag an category routes.  Ready to record.

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -60,7 +60,7 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   try {
-    const categoryData = await User.destroy({
+    const categoryData = await Category.destroy({
       where: {
         id: req.params.id,
       },
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -32,8 +32,8 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new tag
   try {
-    const tagData = await Tag.create();
-    res.status(200).json(locationData);
+    const tagData = await Tag.create(req.body);
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(400).json(err);
   }
@@ -41,7 +41,7 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   try {
-    const tagData = await Category.update(req.body, {
+    const tagData = await Tag.update(req.body, {
       where: {
         id: req.params.id,
       },
@@ -58,7 +58,7 @@ router.put('/:id', async (req, res) => {
 
 router.delete('/:id', async (req, res) => {
   try {
-    const tagData = await User.destroy({
+    const tagData = await Tag.destroy({
       where: {
         id: req.params.id,
       },
